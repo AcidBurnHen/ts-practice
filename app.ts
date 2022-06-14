73e8ba6b08fd3f0cab20496aa0ee99d2a3ed4208
acidburn <cc.acidburn@gmail.com>
2022-06-14 17:40:13 +0200

    Working with basic typescript object

--- app.ts
+++ app.ts
@@ -1,22 +1,11 @@
-function add(n1: number, n2: number, showResult: boolean, phrase: string) {
+/* We don't have to declare the object type as TS will infer it from the variable */
 
-    /* Do the math before adding it onto the string */
-    const result = n1 + n2
-
-    if (showResult) {
-    /* Adding string before numbers n1 + n2 converts everything into a string so the math does not work */
-       console.log(phrase + result)
-    } else {
-        return n1 + n2;
-    }
+const person: {
+    name: string;
+    age: number;
+} = {
+    name: "Marin",
+    age: 25
 }
 
-/* Typescript infers types to variables so we do not have to add them except special cases like 
-using let without assigning a value where we can add an explicit type */
-let number1: number;
-number1 = 5;
-var number2 = 3.2;
-let printResult = true
-var resultPhrase = "Result is: "
-
-add(number1, number2, printResult, resultPhrase)
+console.log(person)
