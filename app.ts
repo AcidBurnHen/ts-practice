b74bba2670c160402e50f54f8b02831c120fdee3
acidburn <cc.acidburn@gmail.com>
2022-06-14 17:02:10 +0200

    Basic addition function in Typescript

--- app.ts
+++ app.ts
@@ -1,1 +1,10 @@
-console.log("test")
+function add(n1: number, n2: number) {
+    return n1 + n2;
+}
+
+var number1 = 5;
+var number2 = 3.2;
+
+var result = add(number1, number2)
+
+console.log(result)
