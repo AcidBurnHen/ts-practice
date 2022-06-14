639e71d593a69bef33363d0f9cdd55d47c6efe84
acidburn <cc.acidburn@gmail.com>
2022-06-14 18:00:21 +0200

    Added tuple type to person object

--- app.ts
+++ app.ts
@@ -1,10 +1,21 @@
 // We don't have to declare the object type as TS will infer it from the variable 
-const person = {
+const person: {
+    name: string;
+    age: number;
+    hobbies: string[];
+    role: [number, string] // This creates a tuple type in TS
+} = {
     name: "Marin",
     age: 25,
-    /*TS infers this array as a type string[] */
-    hobbies: ['Anime', 'Gaming']
+    // TS infers this array as a type string[] 
+    hobbies: ['Anime', 'Gaming'],
+    /* TS infers this as a union type, but we want to use a tuple, because 
+    this should be a fixed lenght array with specific types */
+    role: [2, "author"]
 }
+
+// person.role.push('admin'); won't throw an error because tuples support push method
+// person.role[1] = 10; will throw an error when we use a tuple
 
 /* Tells TS that we plan to store an array of strings inside of this variable, if 
 we want to use different types inside of the array we can use the type any[] */
